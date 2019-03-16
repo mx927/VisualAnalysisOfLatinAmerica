5209067e5efb8c2a571b966f23c93d6f27bc51cd
mx <mx927@foxmail.com>
2019-03-16 23:54:28 +0800

    finish file_helper, rename dir

---
+++ serve/src/util/__init__.py
@@ -0,0 +1,7 @@
+from . import file_helper
+
+# 将file_helper中的方法和变量导出
+path = file_helper.path
+save_csv = file_helper.save_csv
+open_json = file_helper.open_json
+save_json = file_helper.save_json
---
+++ serve/src/util/file_helper.py
@@ -0,0 +1,61 @@
+#coding=utf-8
+import os
+import csv
+import json
+
+class Path:
+    def __init__(self):
+        self.filse=os.getcwd() + '/serve/files/' 
+        self.origin_data=self.filse + 'latin_data/'    
+    
+
+path = Path()
+
+def open_csv(path):
+    if path.find('.csv') == -1:
+        path += '.csv'
+
+    if isFile(path) is False:
+        raise RuntimeError(f'csv文件路径错误：{path}')
+            
+    with open(path,'r') as file:
+        csv_data = csv.reader(file)
+        array = []
+        for line in csv_data:
+            array.append(line)
+        return array
+
+
+def open_json(path):
+    if path.find('.json') == -1:
+        path += '.json'
+
+    if isFile(path) == False:
+        raise RuntimeError(f'json文件路径错误：{path}')
+
+    with open(path,'r') as f:
+        json_data = json.load(f)
+        return json_data
+
+
+def save_csv(data,path):
+    if path.find('.csv') == -1:
+        path += '.csv'
+
+    with open(path, 'w') as file:
+        csv_writer = csv.writer(file)
+        for line in data:
+            print(line)
+            csv_writer.writerow(line)
+
+
+def save_json(data,path):
+    if path.find('.json') == -1:
+        path += '.json'
+
+    with open(path,'w') as f:
+        json.dump(data, f, ensure_ascii=False)
+
+
+def isFile(path):
+    return os.path.isfile(path)
